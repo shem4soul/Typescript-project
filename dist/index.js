fac9b6be69cb7595dbde074d5bdb676ca8a86668
Shittu Emmanuel <shem4soul@gmail.com>
2026-01-14 16:03:37 +0100

    feat: update console log message in index files for clarity

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hello shem of God ");
+console.log("My New House in New York");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-console.log(
-  "hello, ninjas!!! how are you doing today?  I hope you're having a great day!"
-);
+console.log("My New House in New York");
