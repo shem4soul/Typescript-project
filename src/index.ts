a8fdef8c5d9e78e9b1bb05cf7fb4e5c0a6935bb2
Shittu Emmanuel <shem4soul@gmail.com>
2026-01-15 19:42:04 +0100

    feat: add variable declarations and initial values in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 console.log("My New House in West Germany");
+
+let age: number = 30;
+let firstName: string = "Mario";
+let isFictional: boolean;
+
+age = 99;
+firstName = "MySchool";
+isFictional = true;
+
+let planet = "Earth";
+let moons = 1;
+let isLarge = false;
+
+planet = "Saturn";
