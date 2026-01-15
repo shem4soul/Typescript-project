0a179efad13041e21c7a4aca62b54625554f160b
Shittu Emmanuel <shem4soul@gmail.com>
2026-01-15 20:35:38 +0100

    feat: add array and object manipulation examples in index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,53 @@ let moons = 1;
 let isLarge = false;
 
 planet = "Saturn";
+moons = 145;
+isLarge = true;
+
+//null & undefined
+
+let something = null;
+let anotherThing = undefined;
+
+// arrays
+
+let names: string[] = ["Mario", "Luigi", "Peach"];
+let ages: number[] = [25, 28, 24];
+
+names.push("Toad");
+ages.push(34);
+
+//type inference with arrays
+
+let fruits = ["apples", "pears", "bananas", "mangoes"];
+
+fruits.push("grapes");
+
+const f = fruits[3];
+
+let things = [42, true, "hello"];
+
+const t = things[0];
+
+// object literals
+
+let user: { firstName: string; age: number; id: number } = {
+  firstName: "Mario",
+  age: 35,
+  id: 1,
+};
+
+user.firstName = "peach";
+user.id = 8;
+
+//type inferenece with object literals
+
+let person = {
+  name: "luigi",
+  score: 35,
+};
+
+person.name = "shem";
+person.score = 67;
+
+const score = person.score;
