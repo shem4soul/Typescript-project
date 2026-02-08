86033ae78c38e32538a4637625ceca013273fc98
Shittu Emmanuel <shem4soul@gmail.com>
2026-02-08 20:26:27 +0100

    refactor: comment out unused code and add color generation functions

--- dist/index.js
+++ dist/index.js
@@ -1,80 +1,13 @@
 "use strict";
+// console.log("My New House in West Germany");
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("My New House in West Germany");
-let age = 30;
-let firstName = "Mario";
-let isFictional;
-age = 99;
-firstName = "MySchool";
-isFictional = true;
-let planet = "Earth";
-let moons = 1;
-let isLarge = false;
-planet = "Saturn";
-moons = 145;
-isLarge = true;
-//null & undefined
-let something = null;
-let anotherThing = undefined;
-// arrays
-let names = ["Mario", "Luigi", "Peach"];
-let ages = [25, 28, 24];
-names.push("Toad");
-ages.push(34);
-//type inference with arrays
-let fruits = ["apples", "pears", "bananas", "mangoes"];
-fruits.push("grapes");
-const f = fruits[3];
-let things = [42, true, "hello"];
-const t = things[0];
-// object literals
-let user = {
-    firstName: "Mario",
-    age: 35,
-    id: 1,
-};
-user.firstName = "peach";
-user.id = 8;
-//type inferenece with object literals
-let person = {
-    name: "luigi",
-    score: 35,
-};
-person.name = "shem";
-person.score = 67;
-const score = person.score;
-//functions
-function addTwoNumbers(a, b) {
-    return a + b;
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return [r, g, b];
 }
-const subtractTwoNumbers = (a, b) => {
-    return a - b;
-};
-//type inference with functions
-addTwoNumbers(3, 9);
-subtractTwoNumbers(10, 7);
-function addAllNumbers(items) {
-    const total = items.reduce((a, c) => a + c, 0);
-    console.log(total);
-}
-addAllNumbers([5, 7, 9, 11]);
-//return type inference
-function formatGreeting(name, greeting) {
-    return `${greeting}, ${name}`;
-}
-const result = formatGreeting("mario", "hello");
-// any
-let age1;
-age = 25;
-age1 = false;
-//any type in arrays
-let things1 = ["hello", true, 30, null];
-things1.push({ id: 123 });
-function addTogether(value) {
-    return value + value;
-}
-const resultOne = addTogether("hello");
-const resultTwo = addTogether(3);
-// Any is useful when migrating from javaScript to Typescript
-//Beacuse using any wont cause errors initially
+const colorOne = getRandomColor();
+const colorTwo = getRandomColor();
+console.log(colorOne, colorTwo);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,189 +1,189 @@
-console.log("My New House in West Germany");
+// console.log("My New House in West Germany");
 
-let age: number = 30;
-let firstName: string = "Mario";
-let isFictional: boolean;
+// let age: number = 30;
+// let firstName: string = "Mario";
+// let isFictional: boolean;
 
-age = 99;
-firstName = "MySchool";
-isFictional = true;
+// age = 99;
+// firstName = "MySchool";
+// isFictional = true;
 
-let planet = "Earth";
-let moons = 1;
-let isLarge = false;
+// let planet = "Earth";
+// let moons = 1;
+// let isLarge = false;
 
-planet = "Saturn";
-moons = 145;
-isLarge = true;
+// planet = "Saturn";
+// moons = 145;
+// isLarge = true;
 
-//null & undefined
+// //null & undefined
 
-let something = null;
-let anotherThing = undefined;
+// let something = null;
+// let anotherThing = undefined;
 
-// arrays
+// // arrays
 
-let names: string[] = ["Mario", "Luigi", "Peach"];
-let ages: number[] = [25, 28, 24];
+// let names: string[] = ["Mario", "Luigi", "Peach"];
+// let ages: number[] = [25, 28, 24];
 
-names.push("Toad");
-ages.push(34);
+// names.push("Toad");
+// ages.push(34);
 
-//type inference with arrays
+// //type inference with arrays
 
-let fruits = ["apples", "pears", "bananas", "mangoes"];
+// let fruits = ["apples", "pears", "bananas", "mangoes"];
 
-fruits.push("grapes");
+// fruits.push("grapes");
 
-const f = fruits[3];
+// const f = fruits[3];
 
-let things = [42, true, "hello"];
+// let things = [42, true, "hello"];
 
-const t = things[0];
+// const t = things[0];
 
-// object literals
+// // object literals
 
-let user: { firstName: string; age: number; id: number } = {
-  firstName: "Mario",
-  age: 35,
-  id: 1,
-};
+// let user: { firstName: string; age: number; id: number } = {
+//   firstName: "Mario",
+//   age: 35,
+//   id: 1,
+// };
 
-user.firstName = "peach";
-user.id = 8;
+// user.firstName = "peach";
+// user.id = 8;
 
-//type inferenece with object literals
+// //type inferenece with object literals
 
-let person = {
-  name: "luigi",
-  score: 35,
-};
+// let person = {
+//   name: "luigi",
+//   score: 35,
+// };
 
-person.name = "shem";
-person.score = 67;
+// person.name = "shem";
+// person.score = 67;
 
-const score = person.score;
+// const score = person.score;
 
-//functions
-function addTwoNumbers(a: number, b: number): number {
-  return a + b;
-}
+// //functions
+// function addTwoNumbers(a: number, b: number): number {
+//   return a + b;
+// }
 
-const subtractTwoNumbers = (a: number, b: number): number => {
-  return a - b;
-};
+// const subtractTwoNumbers = (a: number, b: number): number => {
+//   return a - b;
+// };
 
-//type inference with functions
-addTwoNumbers(3, 9);
-subtractTwoNumbers(10, 7);
+// //type inference with functions
+// addTwoNumbers(3, 9);
+// subtractTwoNumbers(10, 7);
 
-function addAllNumbers(items: number[]): void {
-  const total = items.reduce((a, c) => a + c, 0);
-  console.log(total);
-}
+// function addAllNumbers(items: number[]): void {
+//   const total = items.reduce((a, c) => a + c, 0);
+//   console.log(total);
+// }
 
-addAllNumbers([5, 7, 9, 11]);
+// addAllNumbers([5, 7, 9, 11]);
 
-//return type inference
+// //return type inference
 
-function formatGreeting(name: string, greeting: string): string {
-  return `${greeting}, ${name}`;
-}
+// function formatGreeting(name: string, greeting: string): string {
+//   return `${greeting}, ${name}`;
+// }
 
-const result = formatGreeting("mario", "hello");
+// const result = formatGreeting("mario", "hello");
 
-// any
-let age1: any;
-age = 25;
+// // any
+// let age1: any;
+// age = 25;
 
-age1 = false;
+// age1 = false;
 
-//any type in arrays
-let things1: any[] = ["hello", true, 30, null];
+// //any type in arrays
+// let things1: any[] = ["hello", true, 30, null];
 
-things1.push({ id: 123 });
+// things1.push({ id: 123 });
 
-function addTogether(value: any): any {
-  return value + value;
-}
+// function addTogether(value: any): any {
+//   return value + value;
+// }
 
-const resultOne = addTogether("hello");
-const resultTwo = addTogether(3);
+// const resultOne = addTogether("hello");
+// const resultTwo = addTogether(3);
 
-// Any is useful when migrating from javaScript to Typescript
-//Beacuse using any wont cause errors initially
+// // Any is useful when migrating from javaScript to Typescript
+// //Beacuse using any wont cause errors initially
 
-// tuples
-let person1: [string, number, boolean] = ["mario", 66, true];
+// // tuples
+// let person1: [string, number, boolean] = ["mario", 66, true];
 
-//tuples Examples
+// //tuples Examples
 
-let hsla: [number, string, string, number];
-hsla = [200, "100%", "50%", 0.5];
+// let hsla: [number, string, string, number];
+// hsla = [200, "100%", "50%", 0.5];
 
-let xy: [number, number];
+// let xy: [number, number];
 
-xy = [94.7, 20.1];
+// xy = [94.7, 20.1];
 
-function useCoords(): [number, number] {
-  // get coords
+// function useCoords(): [number, number] {
+//   // get coords
 
-  const lat = 100;
-  const long = 77;
+//   const lat = 100;
+//   const long = 77;
 
-  return [lat, long];
-}
+//   return [lat, long];
+// }
 
-const [lat, long] = useCoords();
+// const [lat, long] = useCoords();
 
-//---------
-//named tuples
-//----------
+// //---------
+// //named tuples
+// //----------
 
-let user2: [name: string, age: number];
+// let user2: [name: string, age: number];
 
-user2 = ["peach", 25];
+// user2 = ["peach", 25];
 
-console.log(user2[0]);
+// console.log(user2[0]);
 
-//interfaces
+// //interfaces
 
-interface Author {
-  name: string;
-  avatar: string;
-}
+// interface Author {
+//   name: string;
+//   avatar: string;
+// }
 
-const authorOne: Author = { name: "mario", avatar: "/img/mario.png" };
+// const authorOne: Author = { name: "mario", avatar: "/img/mario.png" };
 
-interface Post {
-  title: string;
-  body: string;
-  tags: string[];
-  create_at: Date;
-  author: Author;
-}
+// interface Post {
+//   title: string;
+//   body: string;
+//   tags: string[];
+//   create_at: Date;
+//   author: Author;
+// }
 
-const newPost: Post = {
-  title: "My first post",
-  body: "This is the content of my first post",
-  tags: ["welcome", "firstpost"],
-  create_at: new Date(),
-  author: authorOne,
-};
+// const newPost: Post = {
+//   title: "My first post",
+//   body: "This is the content of my first post",
+//   tags: ["welcome", "firstpost"],
+//   create_at: new Date(),
+//   author: authorOne,
+// };
 
-// as function argument types
+// // as function argument types
 
-function createPost(post: Post): void {
-  console.log(`Created post ${post.title} by ${post.author.name}`);
-}
+// function createPost(post: Post): void {
+//   console.log(`Created post ${post.title} by ${post.author.name}`);
+// }
 
-createPost(newPost);
+// createPost(newPost);
 
-//with arrays
+// //with arrays
 
-let posts: Post[] = [];
+// let posts: Post[] = [];
 
-posts.push(newPost);
+// posts.push(newPost);
 
 //types aliases
 
@@ -202,3 +202,5 @@ function getRandomColor(): Rgb {
 const colorOne = getRandomColor();
 const colorTwo = getRandomColor();
 console.log(colorOne, colorTwo);
+
+//
