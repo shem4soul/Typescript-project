e8b30be5bf2a17985f89514be3d044527752d730
Shittu Emmanuel <shem4soul@gmail.com>
2026-02-11 18:19:29 +0100

    tagged interfaces

--- src/index.ts
+++ src/index.ts
@@ -236,3 +236,25 @@ let anotherId: Id;
 
 anotherId = 123;
 anotherId = "abc123";
+
+//union type pitfalls
+
+//type guards
+
+function swapIdType(id: Id) {
+  //can only use props and methods common to
+  //both number and string types
+  //parseInt(id) --> not allowed because id could be a string or number
+
+  if (typeof id === "string") {
+    //can use string methods here
+    return parseInt(id);
+  } else {
+    //can use number methods and properties here
+    return id.toString();
+  }
+}
+const idOne = swapIdType(1);
+const idTwo = swapIdType("2");
+
+console.log(idOne, idTwo);
